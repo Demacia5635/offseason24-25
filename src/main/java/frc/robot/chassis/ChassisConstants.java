99d5622c162ac75b8463f494727b1ef2c9324501
nanonoam <97607867+nanonoam@users.noreply.github.com>
2024-08-27 08:03:23 +0300

    FF_coretion

--- src/main/java/frc/robot/chassis/ChassisConstants.java
+++ src/main/java/frc/robot/chassis/ChassisConstants.java
@@ -33,7 +33,7 @@ public final class ChassisConstants {
 
   public static final double FRONT_PULSES_PER_DEGREE = FRONT_STEER_RATIO * MOTOR_PULSES_PER_ROTATION / 360.0;
   public static final double BACK_PULSES_PER_DEGREE =  BACK_STEER_RATIO * MOTOR_PULSES_PER_ROTATION / 360.0;
-  
+
   // PID
   public static final PID_Constants MOVE_PID = new PID_Constants(0.085598906233349*10*1023/PULSES_PER_METER, 0, 0);
   public static final PID_Constants FRONT_STEER_PID = new PID_Constants(0.000209225899609*10*1023/FRONT_PULSES_PER_DEGREE, 0, 0);
@@ -44,9 +44,7 @@ public final class ChassisConstants {
   // public static final FF_Constants MOVE_FF = new FF_Constants(0.1496659759518384, 0.405476931680402, 0.02251759948634);
   public static final FF_Constants FRONT_STEER_FF = new FF_Constants(0.069108623637248, 0.00034365326824, 0.000702476229803);
   public static final FF_Constants BACK_STEER_FF = new FF_Constants(0.080821555555163, 0.000529165452406, 0.004994578577863);
-  public static final FF_Constants MOVE_FF_LESS = new FF_Constants(0.1, 0.2, 0.02251759948634);
   public static final FF_Constants MOVE_FF_MORE = new FF_Constants(0.2, 0.223, 0.03251759948634);
-  public static final FF_Constants MOVE_FF_MORE2 = new FF_Constants(1, 0.25, 0.03251759948634);
 
 
 //left front
@@ -73,7 +71,7 @@ public static final double COLLECT_OFFSET_METERS = 0.7;
         .withPID(MOVE_PID.KP, MOVE_PID.KI, MOVE_PID.KD,MOVE_FF_MORE.KS,MOVE_FF_MORE.KV,MOVE_FF_MORE.KA,0)
         .withInvert(false).withMotorRatio(1/MOVE_GEAR_RATIO).withCurrent(8, 10, 0.1),
       new TalonConfig(5,"rio", "FrontLeft/Steer")
-        .withPID(MOVE_PID.KP, MOVE_PID.KI, MOVE_PID.KD,MOVE_FF_MORE.KS,MOVE_FF_MORE.KV,MOVE_FF_MORE.KA,0)
+        .withPID(FRONT_STEER_PID.KP, FRONT_STEER_PID.KI, FRONT_STEER_PID.KD,FRONT_STEER_FF.KS,FRONT_STEER_FF.KV,FRONT_STEER_FF.KA,0)
         .withInvert(false).withMotorRatio(1/FRONT_STEER_RATIO).withCurrent(8, 10, 0.1),
       6,
       new Translation2d(0.332, 0.277),
@@ -86,7 +84,7 @@ public static final double COLLECT_OFFSET_METERS = 0.7;
         .withPID(MOVE_PID.KP, MOVE_PID.KI, MOVE_PID.KD,MOVE_FF_MORE.KS,MOVE_FF_MORE.KV,MOVE_FF_MORE.KA,0)
         .withInvert(false).withMotorRatio(1/MOVE_GEAR_RATIO).withCurrent(8, 10, 0.1),
       new TalonConfig(2,"rio", "FrontRight/Steer")
-        .withPID(MOVE_PID.KP, MOVE_PID.KI, MOVE_PID.KD,MOVE_FF_MORE.KS,MOVE_FF_MORE.KV,MOVE_FF_MORE.KA,0)
+        .withPID(FRONT_STEER_PID.KP, FRONT_STEER_PID.KI, FRONT_STEER_PID.KD,FRONT_STEER_FF.KS,FRONT_STEER_FF.KV,FRONT_STEER_FF.KA,0)
         .withInvert(false).withMotorRatio(1/FRONT_STEER_RATIO).withCurrent(8, 10, 0.1),
       3,
       new Translation2d(0.332, -0.277),
@@ -101,7 +99,7 @@ public static final double COLLECT_OFFSET_METERS = 0.7;
         .withPID(MOVE_PID.KP, MOVE_PID.KI, MOVE_PID.KD,MOVE_FF_MORE.KS,MOVE_FF_MORE.KV,MOVE_FF_MORE.KA,0)
         .withInvert(true).withMotorRatio(1/MOVE_GEAR_RATIO).withCurrent(8, 10, 0.1),
       new TalonConfig(11,"rio", "BackLeft/Steer")
-        .withPID(MOVE_PID.KP, MOVE_PID.KI, MOVE_PID.KD,MOVE_FF_MORE.KS,MOVE_FF_MORE.KV,MOVE_FF_MORE.KA,0)
+        .withPID(BACK_STEER_PID.KP, BACK_STEER_PID.KI, BACK_STEER_PID.KD,BACK_STEER_FF.KS,BACK_STEER_FF.KV,BACK_STEER_FF.KA,0)
         .withInvert(true).withMotorRatio(1/BACK_STEER_RATIO).withCurrent(8, 10, 0.1),
       12,
       new Translation2d(-0.332, 0.288),
@@ -114,7 +112,7 @@ public static final double COLLECT_OFFSET_METERS = 0.7;
         .withPID(MOVE_PID.KP, MOVE_PID.KI, MOVE_PID.KD,MOVE_FF_MORE.KS,MOVE_FF_MORE.KV,MOVE_FF_MORE.KA,0)
         .withInvert(true).withMotorRatio(1/MOVE_GEAR_RATIO).withCurrent(8, 10, 0.1),
       new TalonConfig(8,"rio", "BackRight/Steer")
-        .withPID(MOVE_PID.KP, MOVE_PID.KI, MOVE_PID.KD,MOVE_FF_MORE.KS,MOVE_FF_MORE.KV,MOVE_FF_MORE.KA,0)
+        .withPID(BACK_STEER_PID.KP, BACK_STEER_PID.KI, BACK_STEER_PID.KD,BACK_STEER_FF.KS,BACK_STEER_FF.KV,BACK_STEER_FF.KA,0)
         .withInvert(true).withMotorRatio(1/BACK_STEER_RATIO).withCurrent(8, 10, 0.1),
       9,
       new Translation2d(-0.332, -0.288),
