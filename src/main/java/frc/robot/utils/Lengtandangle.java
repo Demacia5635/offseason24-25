5bf148a55f5697b59790c1b157a8e6603cda9a5d
nanonoam <97607867+nanonoam@users.noreply.github.com>
2024-09-04 09:40:04 +0300

    pp

--- src/main/java/frc/robot/utils/Lengtandangle.java
+++ src/main/java/frc/robot/utils/Lengtandangle.java
@@ -3,12 +3,10 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.utils;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import frc.robot.Constants;
-import java.util.HashMap;
 /** Add your docs here. */
 public class Lengtandangle {
 
