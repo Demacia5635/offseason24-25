6c2470a440dd893dff96aa45e8029340ef56b488
sagi <sagi21805@gmail.com>
2024-09-08 19:36:53 +0300

    added test

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,7 +5,10 @@
 package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
+import frc.robot.SubSystem.TestSubSytem;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.FunctionalCommand;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
@@ -16,16 +19,21 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
-
+  private TestSubSytem testSubSytem;
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
       new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  
+  
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the trigger bindings
     configureBindings();
   }
+
+  //InstantCommand cmd = new InstantCommand();
+  
 
   /**
    * Use this method to define your trigger->command mappings. Triggers can be created via the
---
+++ src/main/java/frc/robot/SubSystem/TestSubSytem.java
@@ -0,0 +1,44 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.SubSystem;
+
+import javax.swing.text.StyleContext.SmallAttributeSet;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.utils.Cancoder;
+import frc.robot.utils.Pigeon;
+
+public class TestSubSytem extends SubsystemBase {
+  /** Creates a new TestSubSytem. */
+  private Cancoder cancoder;
+  private Pigeon pigeon;
+  public TestSubSytem(int CancoderID, String CancoderCanBus, int PigeonID, String PigeonCanBus) {
+    cancoder = new Cancoder(CancoderID, CancoderCanBus);
+    pigeon = new Pigeon(PigeonID, PigeonCanBus);
+  }
+
+  @Override
+  public void periodic() {
+    SmartDashboard.putNumber("getNonAbsPosition", cancoder.getNonAbsPosition());
+    SmartDashboard.putNumber("getAbsDegree", cancoder.getAbsDegree());
+    SmartDashboard.putNumber("getVelocityDegree", cancoder.getVelocityDegree());
+    SmartDashboard.putNumber("getPigeonAngleDegree", pigeon.getPigeonAngleDegree());
+    SmartDashboard.putNumber("getPigeonPitchDegree", pigeon.getPigeonPitchDegree());
+    SmartDashboard.putNumber("getPigeonRollDegree", pigeon.getPigeonRollDegree());
+    SmartDashboard.putNumber("getXVelocityDPS", pigeon.getXVelocityDPS());
+    SmartDashboard.putNumber("getYVelocityDPS", pigeon.getYVelocityDPS());
+    SmartDashboard.putNumber("getZVelocityDPS", pigeon.getZVelocityDPS());
+  }
+  public void resetPigeon(){
+    pigeon.resetPigeon();
+  }
+  public void setOfset(double offset){
+    cancoder.setOfset(offset);
+  }
+  public void setCanCoderClockwise(Boolean boolDirection){
+    cancoder.setCanCoderClockwise(boolDirection);;
+  }
+}
