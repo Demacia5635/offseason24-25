b479376b6608da4ff5d655de667e69d391ae10dc
Ron Shani <ronshan4u@gmail.com>
2024-09-11 23:35:36 +0300

    continue working on telemetry i hate this

--- src/main/java/frc/robot/utils/LogEntry.java
+++ src/main/java/frc/robot/utils/LogEntry.java
@@ -1,15 +1,43 @@
 package frc.robot.utils;
 
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.function.DoubleSupplier;
 
-import edu.wpi.first.networktables.DoublePublisher;
+import com.ctre.phoenix6.StatusCode;
+
+import edu.wpi.first.wpilibj.RobotController;
 
 public abstract class LogEntry {
     protected String name;
-    protected ArrayList<DoublePublisher> publishers;
+    protected HashMap<String, DoubleSupplier> attributes;
 
     protected LogEntry(String name, int dataAmount) {
         this.name = name;
-        publishers = new ArrayList<>();
+        attributes = new HashMap<>();
+    }
+
+    public void log() {
+        for (Entry<String, DoubleSupplier> attrib : attributes.entrySet()) {
+            double v = attrib.getValue().getAsDouble();
+            double time = (long) (RobotController.getFPGATime() * 1000);
+        
+            log(v, time);
+        }
+    }
+
+    public void log(double v) {
+        log(v, 0);
+    }
+
+    public void log(double v, long time) {
+        entry.append(v, time);
+        if (ntPublisher != null) {
+            ntPublisher.set(v);
+        }
+        if(consumer != null) {
+            consumer.accept(v, time);
+        }
+        lastValue = v;
     }
 }
