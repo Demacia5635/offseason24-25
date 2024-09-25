cc4a88af2456d976f414fc4dc77569cbacedfff4
Daniel <danielvfx19@gmail.com>
2024-09-25 20:42:47 +0300

    started TalonMotor

---
+++ src/main/java/frc/robot/utils/TalonMotor.java
@@ -0,0 +1,30 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.utils;
+
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
+import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+/** Add your docs here. */
+public class TalonMotor {
+  TalonFX talon;
+  TalonFXConfiguration talConfig;
+  VelocityVoltage velocityVoltage = new VelocityVoltage(0).withSlot(0);
+  DutyCycleOut dutyCycle = new DutyCycleOut(0);
+  MotionMagicVoltage motionMagicVoltage = new MotionMagicVoltage(0).withSlot(0);
+  public TalonMotor(int ID, String CANBUS){
+    talon = new TalonFX(ID, CANBUS);
+    configMotor();
+  }
+  private void configMotor(){
+    talConfig = new TalonFXConfiguration();
+  }
+public void setBrake(boolean isBrake){
+    
+  }
+}
