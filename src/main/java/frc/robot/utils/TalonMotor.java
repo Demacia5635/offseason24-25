bd9da4f80e3f2bdb20a24d52a55617d4e8692bbd
fastMirta <tamir.szewach@gmail.com>
2024-10-01 18:46:27 +0300

    Motor Class

--- src/main/java/frc/robot/utils/TalonMotor.java
+++ src/main/java/frc/robot/utils/TalonMotor.java
@@ -11,6 +11,8 @@ import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
+import edu.wpi.first.math.geometry.Rotation2d;
+
 /** Add your docs here. */
 public class TalonMotor {
   TalonFX talon;
@@ -22,17 +24,46 @@ public class TalonMotor {
     talon = new TalonFX(ID, CANBUS);
     configMotor();
   }
+
   private void configMotor(){
     talonConfig = new TalonFXConfiguration();
   }
+
   public void setBrake(boolean isBrake){
+    talon.getConfigurator().refresh(talonConfig.MotorOutput);
     talonConfig.MotorOutput.NeutralMode = isBrake ? NeutralModeValue.Brake : NeutralModeValue.Coast;
     talon.getConfigurator().apply(talonConfig.MotorOutput);
   }
+  
   public void setDuty(double power) {
     talon.setControl(dutyCycle.withOutput(power));
   }
-  public void setVelocity(double velocity, double feedForward) {
+
+  public void setVelocityRPS(double velocity) {
     talon.setControl(velocityVoltage.withVelocity(velocity));
+  }
+
+  public void setMotorPositionRotation(double position){
+    talon.setControl(motionMagicVoltage.withPosition(position));  
+  }
+
+  public void setMotorPositionRotation2d(Rotation2d position){
+    talon.setControl(motionMagicVoltage.withPosition(position.getRotations()));
+  }
+
+  public double getPositionRotation() {
+    return talon.getPosition().getValue();
+  }
+
+  public Rotation2d getPositionRotation2d() {
+    return Rotation2d.fromRotations(getPositionRotation());
+  }
+
+  public double getVelocityRotationPerSecond() {
+    return talon.getVelocity().getValue();
+  }
+  
+  public Rotation2d getVelocityRotation2PerSecond() {
+    return Rotation2d.fromRotations(getVelocityRotationPerSecond());
   }
 }
