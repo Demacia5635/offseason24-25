4ec8db17dfd2e46f4b41a079a5e7d8f91fe33a89
Hatulim <tomer10.katz@gmail.com>
2024-10-17 09:37:48 +0300

    update calculations

--- src/main/java/frc/robot/Shooter/utils/ShooterUtils.java
+++ src/main/java/frc/robot/Shooter/utils/ShooterUtils.java
@@ -10,18 +10,22 @@ public class ShooterUtils {
   static double lastVel = 0;
 
   public static double angleToDistance(double angle) {
-    return (2 * ANGLE_CHANGING_VAR.B * Math.cos(angle)
-        + Math.sqrt(4 * Math.pow(ANGLE_CHANGING_VAR.B, 2) * Math.pow(Math.cos(angle), 2)
-            - 4 * (Math.pow(ANGLE_CHANGING_VAR.B, 2) - Math.pow(ANGLE_CHANGING_VAR.A, 2))) / 2)
-        - ANGLE_CHANGING_VAR.C;
-    // return 2*B*Math.cos(angle) + C; if A = B
+    // return (2 * ANGLE_CHANGING_VAR.B * Math.cos(angle)
+    //     + Math.sqrt(4 * Math.pow(ANGLE_CHANGING_VAR.B, 2) * Math.pow(Math.cos(angle), 2)
+    //         - 4 * (Math.pow(ANGLE_CHANGING_VAR.B, 2) - Math.pow(ANGLE_CHANGING_VAR.A, 2))) / 2)
+    //     - ANGLE_CHANGING_VAR.C;
+    // return 2*ANGLE_CHANGING_VAR.B*Math.cos(Math.toRadians(angle)) - ANGLE_CHANGING_VAR.C; 
+    /* if A = B */
+
+    return ((ANGLE_CHANGING_VAR.B * Math.sin(Math.toRadians(180 - 2*angle)))/ (Math.sin(Math.toRadians(angle)))) - ANGLE_CHANGING_VAR.C;
   }
 
   public static double distanceToAngle(double distance) {
-    return Math.acos(
-        ((Math.pow(ANGLE_CHANGING_VAR.A, 2) - Math.pow(ANGLE_CHANGING_VAR.B, 2)) / (ANGLE_CHANGING_VAR.C + distance)
-            - ANGLE_CHANGING_VAR.C - distance) / (-2 * ANGLE_CHANGING_VAR.B));
-    // return Math.acos((distance + C)/(2*B)); if A = B
+    // return Math.acos(
+    //     ((Math.pow(ANGLE_CHANGING_VAR.A, 2) - Math.pow(ANGLE_CHANGING_VAR.B, 2)) / (ANGLE_CHANGING_VAR.C + distance)
+    //         - ANGLE_CHANGING_VAR.C - distance) / (-2 * ANGLE_CHANGING_VAR.B));
+    return Math.toDegrees(Math.acos((distance + ANGLE_CHANGING_VAR.C)/(2*ANGLE_CHANGING_VAR.B))); 
+    /* if A = B */
   }
 
   public static double getUpMotorFF(double vel) {
