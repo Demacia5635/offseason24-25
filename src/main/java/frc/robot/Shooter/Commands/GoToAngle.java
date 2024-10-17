099014f3b73f013bf75308e130602235c3a289c9
Hatulim <tomer10.katz@gmail.com>
2024-10-17 09:31:30 +0300

    update go to angle and shoot commands
    add distance from speaker
    format the code

--- src/main/java/frc/robot/Shooter/Commands/GoToAngle.java
+++ src/main/java/frc/robot/Shooter/Commands/GoToAngle.java
@@ -4,137 +4,163 @@
 
 package frc.robot.Shooter.Commands;
 
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Field;
+import frc.robot.RobotContainer;
 import frc.robot.Shooter.ShooterConstants.AMP_VAR;
 import frc.robot.Shooter.ShooterConstants.ANGLE_CHANGING_VAR;
-import frc.robot.Shooter.ShooterConstants.DISTANCES;
 import frc.robot.Shooter.ShooterConstants.STAGE_VAR;
 import frc.robot.Shooter.ShooterConstants.STATE;
 import frc.robot.Shooter.ShooterConstants.SUBWOFFER_VAR;
 import frc.robot.Shooter.Subsystems.AngleChanger;
 import frc.robot.Shooter.utils.LookUpTable;
 import frc.robot.Shooter.utils.Ready;
+import frc.robot.chassis.subsystems.Chassis;
+import frc.robot.utils.LogManager;
 
 import static frc.robot.Shooter.ShooterConstants.*;
-
 
 public class GoToAngle extends Command {
   /** Creates a new setShooting. */
   private LookUpTable lookupTable;
   private AngleChanger angleChanger;
-  public double wantedAngle = 35;
-  private double testingAngle = 35; 
-  private double distance = 0;
-  public double XDistance = 0;
+  private Chassis chassis;
+
+  private double wantedAngle;
+  private double testingAngle;
+  private double distence;
+  private double distenceX;
 
   public STATE state;
   public static boolean isAngleReady = false;
 
-  
+  private Translation2d speaker;
 
-  public GoToAngle(AngleChanger angleChanger) {
+  public GoToAngle(AngleChanger angleChanger, Chassis chassis) {
     this.angleChanger = angleChanger;
+    this.chassis = chassis;
+    state = angleChanger.angleState;
     lookupTable = angleChanger.lookUpTable;
+
+    wantedAngle = angleChanger.getAngle();
+    testingAngle = angleChanger.getAngle();
+    distence = 0;
+    distenceX = 0;
+
+    speaker = RobotContainer.isRed ? Field.RedSpeaker : Field.Speaker;
+
     SmartDashboard.putData(this);
     addRequirements(angleChanger);
   }
 
   @Override
-  public void initSendable(SendableBuilder builder){
-      builder.addDoubleProperty("Angle", this::getWantedAngle, this::setWantedAngle);
-      
+  public void initSendable(SendableBuilder builder) {
+    builder.addDoubleProperty("Angle", this::getWantedAngle, this::setWantedAngle);
+
+    LogManager.addEntry("Shooter/AngleChanging/Wanted angle", ()-> wantedAngle);
+    LogManager.addEntry("Shooter/AngleChanging/distence from speaker", ()-> speaker.minus(chassis.getPose().getTranslation()).getNorm());
   }
 
-  public double getWantedAngle(){
-      return this.testingAngle;
+  public double getWantedAngle() {
+    return this.testingAngle;
   }
-  
-  public void setWantedAngle(double testingAngle){
-      this.testingAngle = testingAngle;
+
+  public void setWantedAngle(double testingAngle) {
+    this.testingAngle = testingAngle;
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-
+    state = angleChanger.angleState;
+    speaker = RobotContainer.isRed ? Field.RedSpeaker : Field.Speaker;
   }
 
   // Called every time the scheduler runs while the command is scheduled.
-  
-      /**index: 0 - distance, 1 - angle, 2 - up vel, 3 - down vel 
-       * 
-       * @param lookUpTableData index of angle - 0, up vel - 1, down vel - 2
-       * 
-      */
+
+  /**
+   * index: 0 - distance, 1 - angle, 2 - up vel, 3 - down vel
+   * 
+   * @param lookUpTableData index of angle - 0, up vel - 1, down vel - 2
+   * 
+   */
   @Override
   public void execute() {
-    if (XDistance >= DISTANCES.WING_DISTANCE && XDistance < DISTANCES.RIVAL_WING_DISTANCE && state != STATE.AMP && state != STATE.STAGE && state != STATE.SUBWOFFER && state != STATE.TESTING){
-      state = STATE.DELIVERY_MID;
-    }
-    if (XDistance >= DISTANCES.RIVAL_WING_DISTANCE && state != STATE.AMP && state != STATE.STAGE && state != STATE.SUBWOFFER && state != STATE.TESTING){
-      state = STATE.DELIVERY_RIVAL;
-    }
-    switch(state){
+    // distenceX = chassis.getPose().getX();
+    // if (distenceX >= DISTANCES.WING_DISTANCE && distenceX < DISTANCES.RIVAL_WING_DISTANCE && state != STATE.AMP
+    //     && state != STATE.STAGE && state != STATE.SUBWOFFER && state != STATE.TESTING) {
+    //   state = STATE.DELIVERY_MID;
+    // }
+    // if (distenceX >= DISTANCES.RIVAL_WING_DISTANCE && state != STATE.AMP && state != STATE.STAGE
+    //     && state != STATE.SUBWOFFER && state != STATE.TESTING) {
+    //   state = STATE.DELIVERY_RIVAL;
+    // }
+
+    switch (state) {
       case AMP:
-          wantedAngle = AMP_VAR.AMP_ANGLE;
-          break;
-      
+        wantedAngle = AMP_VAR.AMP_ANGLE;
+        break;
+
       case STAGE:
-          wantedAngle = STAGE_VAR.STAGE_ANGLE;
-  
-          break;
+        wantedAngle = STAGE_VAR.STAGE_ANGLE;
+
+        break;
 
       case SUBWOFFER:
-          wantedAngle = SUBWOFFER_VAR.SUBWOFFER_ANGLE;
-          break;
+        wantedAngle = SUBWOFFER_VAR.SUBWOFFER_ANGLE;
+        break;
 
       case SPEAKER:
-           distance = -1;
-           double[] speakerLookUpTableData = lookupTable.get(distance);
-           wantedAngle = speakerLookUpTableData[0];
-          break;
+        distence = speaker.minus(chassis.getPose().getTranslation()).getNorm();
+        double[] speakerLookUpTableData = lookupTable.get(distence);
+        wantedAngle = speakerLookUpTableData[0];
+        break;
 
-     case DELIVERY_MID:
-           distance = -1;
-           double[] deliveryMisLookUpTableData = lookupTable.get(distance);
-           wantedAngle = deliveryMisLookUpTableData[0];
-          break;
+      case DELIVERY_MID:
+        distence = -1;
+        double[] deliveryMisLookUpTableData = lookupTable.get(distence);
+        wantedAngle = deliveryMisLookUpTableData[0];
+        break;
 
-     case DELIVERY_RIVAL:
-           distance = -1;
-           double[] deliveryRivalLookUpTableData = lookupTable.get(distance);
-           wantedAngle = deliveryRivalLookUpTableData[0];
-          break;
+      case DELIVERY_RIVAL:
+        distence = -1;
+        double[] deliveryRivalLookUpTableData = lookupTable.get(distence);
+        wantedAngle = deliveryRivalLookUpTableData[0];
+        break;
 
       case TESTING:
-          wantedAngle = testingAngle;
-          break;
+        wantedAngle = testingAngle;
+        break;
 
       case IDLE:
-        wantedAngle = IDLE_VAR.IDLE_ANGLE;
-          break;
-    }
-    
-    while (angleChanger.getAngle() <= ANGLE_CHANGING_VAR.MIN_ANGLE){
-      angleChanger.setMotorPower(ANGLE_CHANGING_POW.ANGLE_MOTOR_POWER);
-    }
-    while (angleChanger.getAngle() >= ANGLE_CHANGING_VAR.TOP_ANGLE){
-      angleChanger.setMotorPower(-ANGLE_CHANGING_POW.ANGLE_MOTOR_POWER);
+        wantedAngle = angleChanger.getAngle();
+        break;
     }
 
-    isAngleReady = Ready.isAngleReady(wantedAngle);
+    while (state != STATE.IDLE) {
 
-    angleChanger.goToAngle(wantedAngle);
+      while (angleChanger.getAngle() <= ANGLE_CHANGING_VAR.MIN_ANGLE) {
+        angleChanger.setMotorPower(-ANGLE_CHANGING_POW.ANGLE_MOTOR_POWER);
+      }
+      while (angleChanger.getAngle() >= ANGLE_CHANGING_VAR.TOP_ANGLE) {
+        angleChanger.setMotorPower(ANGLE_CHANGING_POW.ANGLE_MOTOR_POWER);
+      }
 
+      isAngleReady = Ready.isAngleReady(wantedAngle);
+
+      // angleChanger.goToAngle(wantedAngle);
+      angleChanger.goToAnglePositionVol(wantedAngle);
+    }
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    
+    angleChanger.setMotorPower(0);
   }
 
   // Returns true when the command should end.
@@ -143,8 +169,4 @@ public class GoToAngle extends Command {
     return false;
   }
 
-
 }
-
-
-// p = ks + kv*v + kv2 + v^2 + ka*a
--- src/main/java/frc/robot/Shooter/Commands/Shoot.java
+++ src/main/java/frc/robot/Shooter/Commands/Shoot.java
@@ -6,13 +6,15 @@ package frc.robot.Shooter.Commands;
 
 import static frc.robot.RobotContainer.*;
 
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Field;
+import frc.robot.RobotContainer;
 import frc.robot.Intake.Subsystem.Intake;
 import frc.robot.Shooter.ShooterConstants.AMP_VAR;
-import frc.robot.Shooter.ShooterConstants.DISTANCES;
 import frc.robot.Shooter.ShooterConstants.SHOOTER_POW;
 import frc.robot.Shooter.ShooterConstants.STAGE_VAR;
 import frc.robot.Shooter.ShooterConstants.STATE;
@@ -20,156 +22,195 @@ import frc.robot.Shooter.ShooterConstants.SUBWOFFER_VAR;
 import frc.robot.Shooter.Subsystems.Shooter;
 import frc.robot.Shooter.utils.LookUpTable;
 import frc.robot.Shooter.utils.Ready;
+import frc.robot.chassis.subsystems.Chassis;
 
 import static frc.robot.Shooter.ShooterConstants.*;
 
 public class Shoot extends Command {
-  
+
   private Shooter shooter;
   private Intake intake;
+  private Chassis chassis;
 
-  private double upMotorVelocity = 0;
-  private double downMotorVelocity = 0;
-  private double testingUpMotorVelocity = 0;
-  private double testingDownMotorVelocity = 0;
-  public STATE state = STATE.TESTING;
-  private double distance = 0;
-  private double XDistance = 0;
+  private double upMotorVelocity;
+  private double downMotorVelocity;
+  private double testingUpMotorVelocity;
+  private double testingDownMotorVelocity;
+
+  public STATE state;
+  private double distence;
+  private double distenceX;
+
   public boolean isReady;
   public boolean isfinished;
 
   private LookUpTable lookupTable;
 
   private Timer shooterTimer;
+  private boolean isTimerRunning;
+
+  private Translation2d speaker;
 
   /** Creates a new Shoot. */
-  public Shoot(Shooter shooter, Intake intake) {
+  public Shoot(Shooter shooter, Intake intake, Chassis chassis) {
     this.shooter = shooter;
     this.intake = intake;
+    this.chassis = chassis;
     lookupTable = shooter.lookUpTable;
+    state = shooter.shooterState;
+
     shooterTimer = new Timer();
+    isTimerRunning = false;
     isReady = false;
     isfinished = false;
+    
+    upMotorVelocity = 0;
+    downMotorVelocity = 0;
+    testingUpMotorVelocity = 0;
+    testingDownMotorVelocity = 0;
+    distence = 0;
+    distenceX = 0;
+
+    speaker = RobotContainer.isRed ? Field.RedSpeaker : Field.Speaker;
+
     SmartDashboard.putData(this);
     addRequirements(shooter, intake);
     // Use addRequirements() here to declare subsystem dependencies.ll
   }
 
   @Override
-  public void initSendable(SendableBuilder builder){
+  public void initSendable(SendableBuilder builder) {
     builder.addDoubleProperty("testingUpMotorVelocity", this::getUpMotorVelocity, this::setUpMotorVelocity);
     builder.addDoubleProperty("testingDownMotorVelocity", this::getDownMotorVelocity, this::setDownMotorVelocity);
   }
 
-    public double getUpMotorVelocity(){
-      return this.testingUpMotorVelocity;
-    }
-  
-    public void setUpMotorVelocity(double testingUpMotorVelocity){
-      this.testingUpMotorVelocity = testingUpMotorVelocity;
-    }
+  public double getUpMotorVelocity() {
+    return this.testingUpMotorVelocity;
+  }
 
-    public double getDownMotorVelocity(){
-      return this.testingDownMotorVelocity;
-    }
-  
-    public void setDownMotorVelocity(double testingDownMotorVelocity){
-      this.testingDownMotorVelocity = testingDownMotorVelocity;
-    }
+  public void setUpMotorVelocity(double testingUpMotorVelocity) {
+    this.testingUpMotorVelocity = testingUpMotorVelocity;
+  }
 
+  public double getDownMotorVelocity() {
+    return this.testingDownMotorVelocity;
+  }
+
+  public void setDownMotorVelocity(double testingDownMotorVelocity) {
+    this.testingDownMotorVelocity = testingDownMotorVelocity;
+  }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    isTimerRunning = false;
+    shooterTimer.stop();
+    shooterTimer.reset();
+    state = shooter.shooterState;
+    speaker = RobotContainer.isRed ? Field.RedSpeaker : Field.Speaker;
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    if (XDistance >= DISTANCES.WING_DISTANCE && XDistance < DISTANCES.RIVAL_WING_DISTANCE && state != STATE.AMP && state != STATE.STAGE && state != STATE.SUBWOFFER && state != STATE.TESTING){
-      state = STATE.DELIVERY_MID;
-    }
-    if (XDistance >= DISTANCES.RIVAL_WING_DISTANCE && state != STATE.AMP && state != STATE.STAGE && state != STATE.SUBWOFFER && state != STATE.TESTING){
-      state = STATE.DELIVERY_RIVAL;
-    }
-    switch(state){
+    // distenceX = chassis.getPose().getX(); 
+    // if (distenceX >= DISTANCES.WING_DISTANCE && distenceX < DISTANCES.RIVAL_WING_DISTANCE && state != STATE.AMP
+    //     && state != STATE.STAGE && state != STATE.SUBWOFFER && state != STATE.TESTING) {
+    //   state = STATE.DELIVERY_MID;
+    // }
+    // if (distenceX >= DISTANCES.RIVAL_WING_DISTANCE && state != STATE.AMP && state != STATE.STAGE
+    //     && state != STATE.SUBWOFFER && state != STATE.TESTING) {
+    //   state = STATE.DELIVERY_RIVAL;
+    // }
+
+    switch (state) {
       case AMP:
-          upMotorVelocity = AMP_VAR.MOTOR_UP_AMP_VELOCITY;
-          downMotorVelocity = AMP_VAR.MOTOR_DOWN_AMP_VELOCITY;
-          break;
+        upMotorVelocity = AMP_VAR.MOTOR_UP_AMP_VELOCITY;
+        downMotorVelocity = AMP_VAR.MOTOR_DOWN_AMP_VELOCITY;
+        break;
 
       case STAGE:
-          upMotorVelocity = STAGE_VAR.MOTOR_UP_STAGE_VELOCITY;
-          downMotorVelocity = STAGE_VAR.MOTOR_DOWN_STAGE_VELOCITY;
-          break;
+        upMotorVelocity = STAGE_VAR.MOTOR_UP_STAGE_VELOCITY;
+        downMotorVelocity = STAGE_VAR.MOTOR_DOWN_STAGE_VELOCITY;
+        break;
 
       case SUBWOFFER:
-          upMotorVelocity = SUBWOFFER_VAR.MOTOR_UP_SUBWOFFER_VELOCITY;
-          downMotorVelocity = SUBWOFFER_VAR.MOTOR_DOWN_SUBWOFFER_VELOCITY;
-          break;
+        upMotorVelocity = SUBWOFFER_VAR.MOTOR_UP_SUBWOFFER_VELOCITY;
+        downMotorVelocity = SUBWOFFER_VAR.MOTOR_DOWN_SUBWOFFER_VELOCITY;
+        break;
 
       case SPEAKER:
-        distance = -1;
-         double[] lookUpTableData = lookupTable.get(distance);
-         upMotorVelocity = lookUpTableData[1];
-         downMotorVelocity = lookUpTableData[2];
+        distence = speaker.minus(chassis.getPose().getTranslation()).getNorm();
+        double[] lookUpTableData = lookupTable.get(distence);
+        upMotorVelocity = lookUpTableData[1];
+        downMotorVelocity = lookUpTableData[2];
         break;
 
-     case TESTING:
-          upMotorVelocity = testingUpMotorVelocity;
-          downMotorVelocity = testingDownMotorVelocity;
-          break;
+      case TESTING:
+        upMotorVelocity = testingUpMotorVelocity;
+        downMotorVelocity = testingDownMotorVelocity;
+        break;
 
       case DELIVERY_MID:
-          distance = -1;
-          double[] deliveryMisLookUpTableData = lookupTable.get(distance);
-          upMotorVelocity = deliveryMisLookUpTableData[1];
-          downMotorVelocity = deliveryMisLookUpTableData[2];
-          break;
+        distence = -1;
+        double[] deliveryMisLookUpTableData = lookupTable.get(distence);
+        upMotorVelocity = deliveryMisLookUpTableData[1];
+        downMotorVelocity = deliveryMisLookUpTableData[2];
+        break;
 
       case DELIVERY_RIVAL:
-          distance = -1;
-          double[] deliveryRivalLookUpTableData = lookupTable.get(distance);
-          upMotorVelocity = deliveryRivalLookUpTableData[1];
-          downMotorVelocity = deliveryRivalLookUpTableData[2];
-          break;
+        distence = -1;
+        double[] deliveryRivalLookUpTableData = lookupTable.get(distence);
+        upMotorVelocity = deliveryRivalLookUpTableData[1];
+        downMotorVelocity = deliveryRivalLookUpTableData[2];
+        break;
 
       case IDLE:
-        upMotorVelocity = IDLE_VAR.MOTOR_UP_IDLE_VELOCITY;
-        downMotorVelocity = IDLE_VAR.MOTOR_DOWN_IDLE_VELOCITY;
+        upMotorVelocity = 0;
+        downMotorVelocity = 0;
         break;
     }
-    
+
     shooter.pidMotorVelocity(upMotorVelocity, downMotorVelocity);
 
     isReady = (Ready.isReady(upMotorVelocity, downMotorVelocity, state) && state != STATE.TESTING)
-        || isShooterReady;
-    if (isReady){
+        || isDriverOverwriteShooter;
+    if (isReady) {
       shooter.setFeedingPower(SHOOTER_POW.FEEDING_MOTOR_POWER);
       intake.motorMoveSetPower(SHOOTER_POW.INTAKE_MOTOR_POWER);
+      // intake.motorPickUpSetPower(0.5);
       isReady = false;
-      isShooterReady = false;
-      if (intake.isNote()){
+      isDriverOverwriteShooter = false;
+
+      if (intake.isNote() && !isTimerRunning) {
         shooterTimer.start();
+        isTimerRunning = true;
       }
-      while (shooterTimer.get()/1000 <= SHOOTER_ATRIBUTES.MIL_SEC_TO_SHOOT){}
+
+      if (shooterTimer.get() * 1000 >= SHOOTER_ATRIBUTES.MIL_SEC_TO_SHOOT) {
         shooterTimer.stop();
+        shooterTimer.reset();
+        isTimerRunning = false;
         isfinished = true;
+      }
     }
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    if (state != STATE.TESTING){
+    if (state != STATE.TESTING) {
       angleChanging.angleState = STATE.SPEAKER;
     }
 
     shooter.setMotorPower(0, 0);
     shooter.setFeedingPower(0);
     intake.motorMoveSetPower(0);
+    shooterTimer.stop();
     shooterTimer.reset();
+    isTimerRunning = false;
     isfinished = false;
-  
+
     if (!interrupted) {
       intake.isNoteInIntake = false;
     }
@@ -178,6 +219,6 @@ public class Shoot extends Command {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return isfinished;
+    return isfinished || state == STATE.IDLE;
   }
 }
